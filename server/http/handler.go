6de2a9edf1efee7ce53f463b4e1713ab2fbed919
ZhangYouXiu <zhangxiaodong011@163.com>
2023-03-02 10:41:21 +0800

    删除无用代码

--- server/http/handler.go
+++ server/http/handler.go
@@ -1,17 +1,9 @@
 package http
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/orglode/navigator/model"
 )
 
 func test(c *gin.Context) {
-	req := model.TestAReq{}
-	if err := c.ShouldBindQuery(&req); err != nil {
-		fmt.Println("asd")
-	}
-	data, _ := svc.Test(req)
-	c.JSON(200, data)
-	return
+
 }
