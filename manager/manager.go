119a979e57058419b19d1882773d8a6b684c1131
zhangxiaodong <beijing@456>
2025-07-10 18:13:05 +0800

    开启地址池-http

--- manager/manager.go
+++ manager/manager.go
@@ -3,6 +3,8 @@ package manager
 import (
 	"github.com/kirinlabs/HttpRequest"
 	"navigator/conf"
+	"net/http"
+	"time"
 )
 
 type Manager struct {
@@ -11,8 +13,19 @@ type Manager struct {
 }
 
 func NewManager(conf *conf.Config) *Manager {
+	transport := &http.Transport{
+		MaxIdleConns:        100,              // 最大空闲连接数
+		MaxIdleConnsPerHost: 10,               // 每个主机最大空闲连接
+		IdleConnTimeout:     90 * time.Second, // 空闲连接超时时间
+		DisableKeepAlives:   false,            // 启用 KeepAlive
+	}
+
+	// 初始化客户端并应用配置
+	client := HttpRequest.NewRequest().
+		Transport(transport).        // 应用自定义 Transport
+		SetTimeout(10 * time.Second) // 设置全局默认超时
 	return &Manager{
 		c:          conf,
-		httpClient: HttpRequest.NewRequest(),
+		httpClient: client,
 	}
 }
